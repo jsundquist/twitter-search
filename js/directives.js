e954862364e36453d94c9b77807cbfb979fa08bc
Jonathan Sundquist <jsundquist@gmail.com>
2013-04-19 20:21:48 -0500

    Adding in hashtag and twitter handle linking

--- js/directives.js
+++ js/directives.js
@@ -1,1 +1,8 @@
 'use strict';
+
+app.directive('directiveLink', function(){
+	console.log('HELLO WORLD');
+	return function(scope, element, attr){
+		console.log(scope);
+	}
+});
--- js/filters.js
+++ js/filters.js
@@ -1,1 +1,14 @@
 'use strict';
+
+app.filter('addLink', function(){
+	
+	return function(input, scope){
+
+		var handleRegex = /(@[a-zA-z0-9_]{1,15})/,
+			hashtag = /(#[a-zA-z][a-zA-z0-9]+)/,
+			matches,
+			qualities = [];
+
+		return input.replace(handleRegex, '<a href="https://twitter.com/$1">$1</a>').replace(hashtag, '<a href="https://twitter.com/search?q=$1&src=hash">$1</a>');
+	}
+});
